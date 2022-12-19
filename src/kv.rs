8428a6fdd9889f291352b77c10360aee1d9259bf
MAKIROR <gzanan@gmail.com>
2022-12-19 23:29:19 +0800

    Update 2022-12-19-23:22

--- src/kv.rs
+++ src/kv.rs
@@ -1,20 +1,47 @@
-use std::fs;
-use std::io::{Error, Write};
+use std::{
+    fs::File,
+    io::{
+        Error,Write
+    },
+    path::PathBuf
+}
+use std::collections::{BTreeMap, HashMap};
+use serde::{Serialize, Deserialize}; 
 
-pub struct DataStore {
-    reader: BufReader<File>,
-    writer: BufWriter<File>,
-    index: HashMap<String, CommandPos>,
+pub enum Command {
+    Add { key: String, value: String },
+    Get { key: String },
+    Remove { key: String },
 }
 
-struct CommandPos {
-    pos: u64,
-    len: u64,
+impl Command {
+    fn add( key: String,value: String ) -> Command {
+        Command::Add { key,value }
+    }
+    fn get( key: String ) -> Command {
+        Command::Get { key }
+    }
+    fn remove( key: String ) -> Command {
+        Command::Remove { key }
+    }
+}
+
+pub struct DataStore {
+    path: PathBuf,
+    rows: Vec<String>
 }
 
 impl DataStore {
     pub fn Open( path : String ) -> Result<Self, Error>  {
-        
+        let contents = fs::read_to_string(path)?;
+        let mut row = Vec::new();
+        for value in contents.lines() {
+            rows.push(value);
+        }
+        Ok(Self {
+            path,
+            rows: row,
+        })
     }
     pub fn CreateFile( path: String ) -> Result<(), Error>  {
         let file_path: Vec<&str> = path.split("/").collect();
@@ -29,6 +56,10 @@ impl DataStore {
         let mut file = fs::File::create(path)?;
         Ok(());
     }
-    pub fn Set(&mut self, key: String, value: String) -> Result<()> {
+    pub fn Get(key: String) -> Result<String> {
+
+    }
+    pub fn Add(&mut self, key: String, value: String) -> Result<()> {
+        let cmd = Command::add(key, value);
     }
 }
