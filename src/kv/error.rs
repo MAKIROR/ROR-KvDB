6140bf66b3a549391382302f5cbfa0b36a424c11
MAKIROR <gzanan@gmail.com>
2022-12-24 23:31:26 +0800

    Update 2022-12-24 23:31

--- src/kv/error.rs
+++ src/kv/error.rs
@@ -1,11 +1,28 @@
 use std::io;
-use failure::Fail;
 
-#[derive(Fail, Debug)]
-pub enum KvError {
-    #[fail(display = "Invalid Path")]
-    InvalidPath,
-    #[fail(display = "Key not found")]
-    KeyNotFound,
+#[macro_use] extern crate quick_error;
 
+quick_error! {
+    #[derive(Debug)]
+    pub enum KvError {
+        IoError( err: io::Error ) {
+            from()
+            display("IO error: {}", err)
+            source(err)
+        }
+        InvalidPath( path: String ) {
+            display("Invalid Path \"{}\"", path)
+        }
+        KeyNotFound( key: String ) {
+            display("Key not found: \"{}\"", key)
+        }
+        SerdeError( err: serde_json::Error ) {
+            from()
+            display("Serde Json Error: {}", err)
+            source(err)
+        }
+        Other(err: Box<std::error::Error>) {
+            cause(&**err)
+        }
+    }
 }
--- src/kv/kv.rs
+++ src/kv/kv.rs
@@ -11,7 +11,7 @@ use std::{
         HashMap
     }
 }
-use serde::{Serialize, Deserialize}; 
+use serde_json; 
 use super::error::KvError;
 
 pub enum Command<T> {
@@ -20,31 +20,42 @@ pub enum Command<T> {
     Delete {key: String },
 }
 
-pub struct DataStore {
-    path: PathBuf,
-    index: HashMap<String, Command>,
+pub struct DataStore<T> {
+    path: String,
+    data: HashMap<String,T>,
 }
 
 impl<T> DataStore<T> {
-    fn Get(&mut self, key: String) -> Result<Option<T>> {
-        match self.index.get(&key) { 
-            None => Err(KvError::KeyNotFound),
+    fn Refresh(&mut self) -> Result<(), KvError> {
+        let content = match fs::read_to_string(path) {
+            Some(c) => c,
+            Err(e) => return Err(KvError::IoError(e)),
+        }
+        content = content.unwrap();
+        let jsondata = match serde_json::from_str(&content) {
+            Some(j) => j,
+            Err(e) => return Err(KvError::SerdeError(e)),
+        }
+        //todo
+    }
+    fn Get(&mut self, key: String) -> Result<Option<T>, KvError> {
+        match self.data.get(&key) { 
+            None => Err(KvError::KeyNotFound(key)),
             Some(c) => {
                 Ok(Some(c.value)),
             }
         }
     }
-    fn Add(&mut self, key: String, value: String) -> Result<()> {
+    fn Add(&mut self, key: String, value: String) -> Result<(), KvError> {
         let content = Command::Add(key,value);
         self.write(content)?;
         Ok(())
     }
     fn Delete(key: String) -> Result<()> {
-
     }
 }
 
-pub fn CreateFile( path: String ) -> Result<(), Error>  {
+pub fn CreateFile( path: String ) -> Result<(), KvError>  {
     let file_path: Vec<&str> = path.split("/").collect();
     if file_path.len() > 1 {
         let mut dir = String::new();
