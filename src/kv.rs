9c35f6c0fb4c976c981c51feeee1c8a2bde27093
MAKIROR <gzanan@gmail.com>
2022-12-28 23:11:41 +0800

    Update 2022-12-28 23:11

--- src/kv.rs
+++ src/kv.rs
@@ -1,6 +1,7 @@
 use std::{
     fs,
     io::Read,
+    path::PathBuf,
     string::String,
     collections::HashMap
 };
@@ -8,60 +9,44 @@ use serde_json;
 use serde::{Deserialize, Serialize};
 use super::error::{KvError,Result};
 
-#[derive(Serialize, Deserialize,Clone)]
-enum Value {
-    Null,
-    Bool(bool),
-    String(String),
-    Array(Vec<Value>),
-    HashMap(HashMap<String, Value>),
-}
-
-#[derive(Serialize, Deserialize)]
 pub enum Command {
-    Get = 0,
-    Add = 1,
-    Delete = 2,
+    Get,
+    Add,
+    Delete,
 }
 
-#[derive(Serialize, Deserialize)]
 pub struct Entry {
-    cmd: Command,
-    key: String,
-    value: Value,
+    command: Command,
+    key_size: usize,
+    value_size: usize,
+    key: Vec<u8>,
+    value: Vec<u8>,
+}
+
+impl Entry {
+    pub fn encode(&self) -> Vec<u8> {
+    }
+    pub fn decode(&self) -> Result<Entry> {
+    }
 }
 
 pub struct DataStore {
     path: String,
-    data: HashMap<String, Value>,
+    file_size: u64,
+    data: HashMap<String, u64>,
 }
 
 impl DataStore {
     pub fn open(path: String) -> Result<DataStore> {
         todo!()
     }
-    fn load(path: String) -> Result<HashMap<String,Value>> {
-        let mut file = std::fs::File::open(path).unwrap();
-        let mut content = String::new();
-        file.read_to_string(&mut content)?;
-        let data: Vec<Entry> = serde_json::from_str(&content)?;
-        let mut result = HashMap::new();
-        for row in &data {
-            match row.cmd {
-                Command::Add => {
-                    todo!()
-                },
-                Command::Delete => result.remove(&row.key),
-                _ => todo!(),
-            };
-        }
-        Ok(result)
-    }
-
-    fn add(&mut self, key: String, value: Value) -> Result<()> {
+    fn add(&mut self, key: &[u8], value: &[u8]) -> Result<()> {
         todo!()
     }
-    fn delete(key: String) -> Result<()> {
+    fn delete(key: &[u8]) -> Result<()> {
+        todo!()
+    }
+    fn write(&mut self, entry: Entry) -> Result<()> {
         todo!()
     }
 }
